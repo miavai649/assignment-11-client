4538fb4de128954b4e0968f222f1a0cdd702920a
Mahamudul Haque Noor <mahamudulhaquenoor@gmail.com>
2022-11-09 01:28:13 +0600

    footer added

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import logo from '../../assets/logo/footerLogo.jpg';
 
 const Footer = () => {
-    return (
-        <div>
-            <h2>this is footer</h2>
+  return (
+    <footer className="footer p-10 bg-black text-neutral-content">
+      <div>
+        <img className="w-24" src={logo} alt="" />
+        <p className="text-2xl">
+          My Cake House
+          <br />
+          Providing reliable tech since 1992
+        </p>
+      </div>
+      <div>
+        <span className="footer-title">Social</span>
+        <div className="grid grid-flow-col align-middle gap-4">
+          <FaFacebook className=" btn btn-circle"></FaFacebook>
+          <FaTwitter className=" btn btn-circle"></FaTwitter>
+          <FaLinkedin className=" btn btn-circle"></FaLinkedin>
+          <FaInstagram className=" btn btn-circle"></FaInstagram>
         </div>
-    );
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
